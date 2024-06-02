a145f47868372831ba15bfacc54e23b343780c2b
Almaaz khan <khan.almaaz1055@gmail.com>
2024-06-03 04:34:46 +0530

    scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,8 @@ import Footer from "./components/Footer";
 import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
-    multiplier: 1.4, // Effect Multiplier
-    reloadOnContextChange: true,
-    touchMultiplier: 2,
-    smoothMobile: 1,
-    smartphone: {
-      smooth: !0,
-      breakpoint: 767,
-    },
-    tablet: {
-      smooth: !1,
-      breakpoint: 1024,
-    },
-  });
+  const scroll = new LocomotiveScroll();
+
   return (
     <div className="w-full min-h-screen text-white bg-zinc-900 overflow-hidden  ">
       <Navbar />
