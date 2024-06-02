863ddea8975f44fdab338bb197b2a76a191401af
Almaaz khan <khan.almaaz1055@gmail.com>
2024-06-03 04:32:20 +0530

    scroll issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import LocomotiveScroll from "locomotive-scroll";
+import React from "react";
 import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
@@ -8,33 +7,28 @@ import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
-  useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: document.querySelector("[data-scroll-container]"),
-      smooth: true,
-      lerp: 0.03,
-      multiplier: 1.4,
-      reloadOnContextChange: true,
-      touchMultiplier: 2,
-      smoothMobile: 0,
-      smartphone: {
-        smooth: !0,
-        breakpoint: 767,
-      },
-      tablet: {
-        smooth: !1,
-        breakpoint: 1024,
-      },
-    });
-  }, []);
-
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+    lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
+    multiplier: 1.4, // Effect Multiplier
+    reloadOnContextChange: true,
+    touchMultiplier: 2,
+    smoothMobile: 1,
+    smartphone: {
+      smooth: !0,
+      breakpoint: 767,
+    },
+    tablet: {
+      smooth: !1,
+      breakpoint: 1024,
+    },
+  });
   return (
-    <div
-      className="w-full min-h-screen text-white bg-zinc-900 overflow-hidden"
-      data-scroll-container
-    >
+    <div className="w-full min-h-screen text-white bg-zinc-900 overflow-hidden  ">
       <Navbar />
       <LandingPage />
       <Marquee />
